f64fa919cdc4415826a989b3bc82a31d4781763c
Vitalii Stankov <36083562+vitaliistankov@users.noreply.github.com>
2022-06-01 11:24:48 +0300

    last commit

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,14 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+
+const x = '23';
+
+if (x === 23) console.log(x);
+
+const calcAge = birthyear => 2037 - birthyear;
+console.log(calcAge(1991));
+
+
+
+
